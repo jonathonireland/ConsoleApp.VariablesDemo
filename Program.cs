05d2d91a125c984119a3c7ede12fe3c8488d770b
Jonathon Ireland <59446203+jonathonireland@users.noreply.github.com>
2024-05-15 11:05:06 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Different datatypes
+/*
+ * 
+ * text - string
+ * integer - int
+ * decimal - double, float, decimal
+ * logical - bool
+ * 
+ */
+
+string name = "Trevoir Williams";
+Console.WriteLine(name);
+Console.WriteLine("I am "+ name); // String concatenation
+Console.WriteLine("They call me {name}"); // String interpolation
+Console.WriteLine("I was given the name {0}", name); // Formatted String
+
+int age = 54;
+Console.WriteLine("My age is: " + age);
+
