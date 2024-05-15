2c93dd58ea21917af34f3423b77ecf93b2e57f7a
Jonathon Ireland <59446203+jonathonireland@users.noreply.github.com>
2024-05-15 11:10:16 -0400

    end of video #17

--- Program.cs
+++ Program.cs
@@ -17,5 +17,13 @@ Console.WriteLine("They call me {name}"); // String interpolation
 Console.WriteLine("I was given the name {0}", name); // Formatted String
 
 int age = 54;
+int retirementYearsLeft = 11;
+int retirementAge = age + retirementYearsLeft;
+
 Console.WriteLine("My age is: " + age);
+Console.WriteLine("My retirement age is: " + retirementAge);
+
+bool isRetired = false;
+
+Console.WriteLine("Am I retired? " + isRetired);
 
